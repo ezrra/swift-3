aae91f76a1342f1ff6e6c8f75e65a210ba641d6d
Israel Martinez <israel.martinez.vargas@gmail.com>
2017-09-28 10:16:22 -0700

    Basic

---
+++ basic.swift
@@ -0,0 +1,22 @@
+print("Hello")
+
+var myVariable = 42 // variable (can't be nil)
+
+let pi = 3.1416 // Constant
+
+let (x, y) = (5, 6) // Asign x = 5, y = 6
+
+let expDouble: Double = 1_000.00_1 // Value = 1,000.0001
+
+let label = "some " + String(myVariable) // Casting
+
+let piText = "Pi = \pi" // String interpolation
+
+var optionString: String? = "optional" // Can be nill
+
+optionString = nil
+
+// Comments
+
+/* multiline comments 
+*/
