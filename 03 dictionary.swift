ba27ee03b6893458b8866fcf6f0ae7351cea3333
Israel Martinez <israel.martinez.vargas@gmail.com>
2017-09-28 11:00:13 -0700

    Dictionary

---
+++ 03 dictionary.swift
@@ -0,0 +1,10 @@
+// Dictionary
+
+var occupation =[
+    "John": "Enginner",
+    "Kevin": "Product Owner"
+]
+
+occupation["Other"] = "Scrum master"
+
+var emptyDictionary = Dictionary<String, Float>()
